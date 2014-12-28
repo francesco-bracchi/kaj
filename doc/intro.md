8aa2cd78c6d3d8b3329d27b08a9678a743649f0d
francesco <frbracch@gmail.com>
2014-12-28 14:11:31 +0100

    new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kaj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kaj/core_test.clj
@@ -0,0 +1,7 @@
+(ns kaj.core-test
+  (:require [clojure.test :refer :all]
+            [kaj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
